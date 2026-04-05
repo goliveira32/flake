207f8ae41a8c9bed01cf6b878d70be08a98d3b7e
goliveira32 <goncalo.oliveira@tuta.com>
2026-04-05 16:12:40 +0100

    feat: disable bootloader menu on startup

--- core/modules/boot.nix
+++ core/modules/boot.nix
@@ -1,6 +1,9 @@
 { ... }: {
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    timeout = 0;
+  };
 
 }
