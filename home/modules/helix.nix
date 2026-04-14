51a7d98c4e1a0497c5b9ce803208ad8daf92c2df
goliveira32 <goncalo.oliveira@tuta.com>
2026-04-14 15:23:02 +0100

    feat: configure Helix for Rust development

--- home/modules/helix.nix
+++ home/modules/helix.nix
@@ -1,18 +1,31 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    languages.language = [
-      {
-        name = "markdown";
-        soft-wrap.enable = true;
-      }
-      {
-        name = "typst";
-        soft-wrap.enable = true;
-      }
-    ];
+    languages = {
+      language-server.rust-analyzer = with pkgs; {
+        command = "${rust-analyzer}/bin/rust-analyzer";
+      };
+      language = [
+        {
+          name = "markdown";
+          soft-wrap.enable = true;
+        }
+        {
+          name = "typst";
+          soft-wrap.enable = true;
+        }
+        {
+          name = "rust";
+          auto-format = true;
+          formatter = with pkgs; {
+            command = "${rustfmt}/bin/rustfmt";
+          };
+          language-servers = [ "rust-analyzer" ];
+        }
+      ];
+    };
     settings.theme = "custom";
     themes.custom = {
       inherits = "catppuccin_mocha";
