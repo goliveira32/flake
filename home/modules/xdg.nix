2a9404a966924339ae46a9852351d8e5ab301d43
goliveira32 <goncalo.oliveira@tuta.com>
2026-05-27 22:00:48 +0100

    feat: add system handlers for images and PDFs

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -2,12 +2,28 @@
 {
 
   xdg = {
+    desktopEntries = {
+      pdf-handler = {
+        name = "Default PDF Handler";
+        exec = "${pkgs.papers}/bin/papers %F";
+        noDisplay = true;
+      };
+      image-handler = {
+        name = "Default Image Handler";
+        exec = "${pkgs.loupe}/bin/loupe %F";
+        noDisplay = true;
+      };
+    };
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "application/pdf" = "org.gnome.Papers.desktop";
-        "image/png" = "org.gnome.Loupe.desktop";
-        "image/jpeg" = "org.gnome.Loupe.desktop";
+        "application/pdf" = "pdf-handler.desktop";
+        "image/gif" = "image-handler.desktop";
+        "image/jpeg" = "image-handler.desktop";
+        "image/png" = "image-handler.desktop";
+        "image/svg+xml" = "image-handler.desktop";
+        "image/svg+xml-compressed" = "image-handler.desktop";
+        "image/webp" = "image-handler.desktop";
       };
     };
     portal = {
