d28c0689c95e10269f89c279132ac42435911789
Gonçalo Oliveira <goncalo.oliveira@tuta.com>
2026-06-27 17:40:12 +0100

    feat: enable Amberol

--- core/modules/software.nix
+++ core/modules/software.nix
@@ -3,6 +3,7 @@
 {
   programs.hyprland.enable = true;
   programs.hyprlock.enable = true;
+  programs.dconf.enable = true;
 
   services.flatpak.enable = true;
   services.udisks2.enable = true;
--- home/modules/default.nix
+++ home/modules/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./alacritty.nix
+    ./amberol.nix
     ./bash.nix
     ./dconf.nix
     ./dunst.nix
